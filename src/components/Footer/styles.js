fbe87b8ef6bf38ecd9f9f99234a8f3c5d68e621e
jordaoqualho <jordaoqualho@gmail.com>
2022-08-21 01:29:23 -0300

    footer

--- src/components/Footer/styles.js
+++ src/components/Footer/styles.js
@@ -7,10 +7,58 @@ export const Container = styled.div`
 `;
 
 export const Wrapper = styled.div`
-  flex-direction: column;
-  display: flex;
+  justify-content: space-between;
+  padding: 100px 0;
   margin: 0 auto;
+  display: flex;
   width: 1240px;
   height: 100%;
-  padding: 100px 0;
+`;
+
+export const Text = styled.div`
+  width: 400px;
+
+  h1 {
+    font-size: var(--large);
+    margin-bottom: 5px;
+
+    span {
+      color: var(--primary);
+    }
+  }
+
+  p {
+    font-size: var(--small);
+  }
+`;
+
+export const Navigation = styled.div`
+  display: flex;
+  gap: 70px;
+
+  .column {
+    h1 {
+      font-size: var(--large);
+      margin-bottom: 10px;
+    }
+
+    p {
+      font-size: var(--small);
+      cursor: pointer;
+      margin-bottom: 5px;
+
+      :hover {
+        color: var(--primary);
+      }
+    }
+
+    img {
+      margin-right: 10px;
+      cursor: pointer;
+
+      :hover {
+        opacity: 0.75;
+      }
+    }
+  }
 `;
